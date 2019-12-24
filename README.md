fdbecb2b619f9a5400a3f4edefcb599144c5b6ad
Michael Hammond <mickeymond@gmail.com>
2019-12-24 08:59:50 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#### This is a Face Detection Hack with Face-Api.js
---
+++ app.js
@@ -0,0 +1,46 @@
+const video = document.getElementById('video');
+
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri('/weights'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/weights'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/weights'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/weights'),
+  faceapi.nets.ageGenderNet.loadFromUri('/weights'),
+]).then(startVideo);
+
+function startVideo() {
+  navigator.getUserMedia(
+    { video: {} },
+    stream => video.srcObject = stream,
+    console.log
+  );
+}
+
+video.addEventListener('play', e => {
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+  const displaySize = { width: video.width, height: video.height }
+  faceapi.matchDimensions(canvas, displaySize);
+
+  setInterval(async () => {
+    const detections = await faceapi.detectAllFaces(
+      video,
+      new faceapi.TinyFaceDetectorOptions()
+    )
+    .withFaceLandmarks()
+    .withFaceExpressions()
+    .withAgeAndGender();
+
+    const resizeDetections = faceapi.resizeResults(detections, displaySize);
+
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+    faceapi.draw.drawDetections(canvas, resizeDetections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizeDetections);
+    faceapi.draw.drawFaceExpressions(canvas, resizeDetections);
+    resizeDetections.forEach(detection => {
+      const box = detection.detection.box
+      const drawBox = new faceapi.draw.DrawBox(box, { label: Math.round(detection.age) + " year old " + detection.gender })
+      drawBox.draw(canvas)
+    })
+  }, 100);
+})
